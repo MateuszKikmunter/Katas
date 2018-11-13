3667b57cd55819d20cd1c0100ae78d69136874b6
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-11-13 12:25:17 +0000

    SmileysCounter

---
+++ Katas.Tests/SmileysCounterTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Katas.Challenges;
+using Xunit;
+
+namespace Katas.Tests
+{
+    public class SmileysCounterTests
+    {
+        [Fact]
+        public void CountSmileys_OneValidFace_ShouldReturnCorrectNumber()
+        {
+            //arrange
+            var input = new[] { ";]", ":[", ";*", ":$", ";-D" };
+            var expected = 1;
+
+            //act
+            var result = SmileysCounter.CountSmileys(input);
+
+            //assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CountSmileys_TwoValidFaces_ShouldReturnCorrectNumber()
+        {
+            //arrange
+            var input = new [] {":)", ";(", ";}", ":-D"};
+            var expected = 2;
+
+            //act
+            var result = SmileysCounter.CountSmileys(input);
+
+            //assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CountSmileys_ThreeValidFaces_ShouldReturnCorrectNumber()
+        {
+            //arrange
+            var input = new[] { ";D", ":-(", ":-)", ";~)" };
+            var expected = 3;
+
+            //act
+            var result = SmileysCounter.CountSmileys(input);
+
+            //assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Katas/Challenges/SmileysCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Katas.Challenges
+{
+    public static class SmileysCounter
+    {
+        /// <summary>
+        /// Given an array (arr) as an argument complete the function CountSmileys that should return the total number of smiling faces.
+        /// -Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+        /// -A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+        /// -Every smiling face must have a smiling mouth that should be marked with either ) or D.
+        /// No additional characters are allowed except for those mentioned.
+        /// Valid smiley face examples:
+        /// :) :D ;-D :~)
+        /// Invalid smiley faces:
+        /// ;( :&gt; :} :] 
+        /// </summary>
+        /// <param name="smileys"></param>
+        /// <returns></returns>
+        public static int CountSmileys(string[] smileys)
+        {
+            var withoutNose = new Regex("^[:;]+[D)]");
+            var withNose = new Regex("^[:;]+[-~]+[D)]");
+            return smileys.Count(smiley => withoutNose.IsMatch(smiley) || withNose.IsMatch(smiley));
+        }
+    }
+}
