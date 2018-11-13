24ec52f2587f03e614131853f7f30dd372de120e
m.kikmunter <M.Kikmunter@gmail.com>
2018-11-13 21:44:12 +0000

    added GetMiddleCharacter

---
+++ Katas.Tests/GetMiddleCharacterTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Katas.Challenges;
+using Xunit;
+
+namespace Katas.Tests
+{
+    public class GetMiddleCharacterTests
+    {
+        [Fact]
+        public void GetMiddle_Odd_ShouldReturnCorrectString()
+        {
+            //arrange
+            var input = "abcdefg";
+
+            //act
+            var result = GetMiddleCharacter.GetMiddle(input);
+
+            //assert
+            result.Should().Be("d");
+        }
+
+        [Fact]
+        public void GetMiddle_Even_ShouldReturnCorrectString()
+        {
+            //arrange
+            var input = "abcdef";
+
+            //act
+            var result = GetMiddleCharacter.GetMiddle(input);
+
+            //assert
+            result.Should().Be("cd");
+        }
+    }
+}
---
+++ Katas/Challenges/GetMiddleCharacter.cs
@@ -0,0 +1,24 @@
+﻿namespace Katas.Challenges
+{
+    /// <summary>
+    /// You are going to be given a word. 
+    /// Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+    /// Examples:
+    /// Kata.getMiddle("test") should return "es"
+    /// 
+    /// Kata.getMiddle("testing") should return "t"
+    /// 
+    /// Kata.getMiddle("middle") should return "dd"
+    /// 
+    /// Kata.getMiddle("A") should return "A"
+    /// </summary>
+    public static class GetMiddleCharacter
+    {
+        public static string GetMiddle(string s)
+        {
+            var sLength = s.Length;
+            var startIndex = sLength / 2;
+            return sLength % 2 == 0 ? s.Substring(startIndex - 1, 2) : s.Substring(startIndex, 1);
+        }
+    }
+}
