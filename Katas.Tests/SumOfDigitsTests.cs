52a086182aff04d55d8c8e6c5d9507fd4297f4fb
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2018-11-14 14:25:25 +0000

    added sum of digits

---
+++ Katas.Tests/SumOfDigitsTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Katas.Challenges;
+using Xunit;
+
+namespace Katas.Tests
+{
+    public class SumOfDigitsTests
+    {
+        [Fact]
+        public void DigitalRoot_SmallNumber_ShouldReturnCorrectValue()
+        {
+            //arrange
+            var input = 16;
+
+            //act
+            var result = SumOfDigits.DigitalRoot(input);
+
+            //assert
+            result.Should().Be(7);
+        }
+
+        [Fact]
+        public void DigitalRoot_ThreeDigitNumber_ShouldReturnCorrectValue()
+        {
+            //arrange
+            var input = 942;
+
+            //act
+            var result = SumOfDigits.DigitalRoot(input);
+
+            //assert
+            result.Should().Be(6);
+        }
+
+        [Fact]
+        public void DigitalRoot_OverHundredThousandNumber_ShouldReturnCorrectValue()
+        {
+            //arrange
+            var input = 132189;
+
+            //act
+            var result = SumOfDigits.DigitalRoot(input);
+
+            //assert
+            result.Should().Be(6);
+        }
+
+        [Fact]
+        public void DigitalRoot_HalfMillion_ShouldReturnCorrectValue()
+        {
+            //arrange
+            var input = 493193;
+
+            //act
+            var result = SumOfDigits.DigitalRoot(input);
+
+            //assert
+            result.Should().Be(2);
+        }
+    }
+}
---
+++ Katas/Challenges/SumOfDigits.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Katas.Challenges
+{
+    public static class SumOfDigits
+    {
+        /// <summary>
+        /// A digital root is the recursive sum of all the digits in a number. 
+        /// Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. 
+        /// This is only applicable to the natural numbers.
+        /// Here's how it works:
+        /// digital_root(16)
+        /// => 1 + 6
+        /// => 7
+        /// 
+        /// digital_root(942)
+        /// => 9 + 4 + 2
+        /// => 15 ...
+        /// => 1 + 5
+        /// => 6
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int DigitalRoot(long n)
+        {
+            while (n >= 10)
+            {
+                n = n.ToString().Select(c => int.Parse(c.ToString())).Sum();
+            }
+            return (int)n;
+        }
+    }
+}
