f2abb7774e448fbaeb3763c7c7ba52385f60b283
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2019-03-06 13:47:17 +0000

    added your order challenge

---
+++ Katas.Tests/YourOrderTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Katas.Challenges;
+using Xunit;
+
+namespace Katas.Tests
+{
+    public class YourOrderTests
+    {
+        [Fact]
+        public void GetOrder_EmptyString_ShouldReturnEmptyString()
+        {
+            //arrange
+            var input = string.Empty;
+
+            //act
+            var result = YourOrder.GetOrder(input);
+
+            //assert
+            result.Should().Be(input);
+        }
+
+        [Fact]
+        public void GetOrder_SimpleTest_ShouldReturnCorrectValue()
+        {
+            //arrange
+            var input = "is2 Thi1s T4est 3a";
+
+            //act
+            var result = YourOrder.GetOrder(input);
+
+            //assert
+            result.Should().Be("Thi1s is2 3a T4est");
+        }
+
+        [Fact]
+        public void GetOrder_SimpleTest1_ShouldReturnCorrectValue()
+        {
+            //arrange
+            var input = "4of Fo1r pe6ople g3ood th5e the2";
+
+            //act
+            var result = YourOrder.GetOrder(input);
+
+            //assert
+            result.Should().Be("Fo1r the2 g3ood 4of th5e pe6ople");
+        }
+    }
+}
