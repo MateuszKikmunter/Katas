56e1d89f8abde7ef7ada86a86e4ca170142cf63a
Mateusz Kikmunter <mateusz.kikmunter@willistowerswatson.com>
2019-03-14 14:21:44 +0000

    added frog-river-one and your-order

---
+++ Katas/Challenges/FrogRiverOne.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas.Challenges
+{
+    public static class FrogRiverOne
+    {
+        public static int Frog(int X, int[] A)
+        {
+            var storage = new Dictionary<int, bool>();
+            for (var i = 0; i < A.Length; i++)
+            {
+                if (!storage.ContainsKey(A[i]))
+                {
+                    storage[i] = true;
+                }
+
+                if (storage.Count() == X)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
